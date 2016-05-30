5c30c88710387a486af62b88a7a77132326ad0a0
Lambert Zhang <tyouteis@gmail.com>
2016-05-30 13:41:19 +0000

    Initialize the repository

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+
+  def account_activation(user)
+    @user = user
+    mail to: user.email, subject: "Account activation"
+  end
+
+  def password_reset(user)
+    @user = user
+    mail to: "zhang@example.com"
+  end
+end
